bd131916c94b48ab54cead3b3414ea5630f166e3
scorbin1 <corbin.sl@gmail.com>
2025-02-14 22:36:51 -0600

    changed operator to more preferred option as per pdk

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe 'ejabberd' do
   context 'with defaults for all parameters' do
-    it { should contain_class('ejabberd') }
+    it { is_expected.to contain_class('ejabberd') }
   end
 end
