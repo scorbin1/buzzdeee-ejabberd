d4465d6f4c0760ad19af834b97e4b661f4e3b2fb
scorbin1 <corbin.sl@gmail.com>
2025-02-15 01:10:50 -0600

    set data types for parameters of class and document

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,10 +6,29 @@
 # @summary Sets up ejabberd service
 #
 #
+# === Parameters
+#
+# @param service_ensure
+#   Type: String
+#   Default Value: defined by calling class
+#   Description: Desired ejabberd service resource status. This parameter
+#                accepts valid service resource ensure values. See service
+#                resource documentation for more information
+#
+# @param service_enable
+#   Type: Boolean
+#   Default Value: defined by calling class
+#   Description: Whether service should be started on boot
+#
+# @param service_flags
+#   Type: String
+#   Default Value: defined by calling class
+#   Description: String of parameters to pass to the service when starting
+#
 class ejabberd::service (
-  $service_ensure,
-  $service_enable,
-  $service_flags,
+  String  $service_ensure,
+  Boolean $service_enable,
+  String  $service_flags,
 ) {
   service { 'ejabberd':
     ensure => $service_ensure,
